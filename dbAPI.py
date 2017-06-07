af05cc081cb09d0565c9413d562e0b81deb13c62
NicolasBravo94 <nicolasbravo94@gmail.com>
2017-06-07 16:31:59 -0400

    suma al agregar deuda y resta al pagar deuda

--- dbAPI.py
+++ dbAPI.py
@@ -9,8 +9,30 @@ def agregar_usuario(user_name):
     return db.insert(query)
     
 def agregar_deuda(deudor_name, prestador_name, monto):
-    query = "INSERT INTO deuda (prestadorName, deudorName, monto) VALUES ('%s', '%s', %d);" % (prestador_name, deudor_name, monto)
-    return db.insert(query)
+    deuda = obtener_deuda(prestador_name, deudor_name)
+    if len(deuda) == 1:
+        nueva_deuda = deuda[0] + monto
+        query = "UPDATE deuda SET monto=%d WHERE prestadorName='%s' AND deudorName='%s';" %(nueva_deuda, prestador_name, deudor_name)
+        return db.insert(query)
+    else:
+        query = "INSERT INTO deuda (prestadorName, deudorName, monto) VALUES ('%s', '%s', %d);" % (prestador_name, deudor_name, monto)
+        return db.insert(query)
+        
+def pagar_deuda(deudor_name, prestador_name, monto):
+    deuda = obtener_deuda(prestador_name, deudor_name)
+    if len(deuda) == 1:
+        nueva_deuda = deuda[0] - monto
+        if (nueva deuda <= 0):
+            query = "DELETE FROM deuda WHERE prestadorName='%s' AND deudorName='%s';" % (prestador_name, deudor_name)
+        else:
+            query = "UPDATE deuda SET monto=%d WHERE prestadorName='%s' AND deudorName='%s';" %(nueva_deuda, prestador_name, deudor_name)
+        return db.insert(query)
+    else: #No habia deuda, error
+        return -1
+        
+def obtener_deuda(prestador_name, deudor_name):
+    query = "SELECT monto FROM deuda WHERE prestadorName='%s' AND deudorName='%s';" % (pretador_name, deudor_name)
+    return db.get(query)
     
 def consultar_deuda(deudor_name):
     query = "SELECT * FROM deuda WHERE deudorName='%s';" % deudor_name
@@ -18,7 +40,6 @@ def consultar_deuda(deudor_name):
     
 def consultar_deudores(prestador_name):
     query = "SELECT * FROM deuda WHERE prestadorName='%s';" % prestador_name
-    print query
     return db.get(query)
 
 def obtener_usuarios():
